5e0a3ed6ff7e4367aba51657d44a6f5f42070810
elias <eliazzlima@gmail.com>
2023-10-16 11:47:10 -0300

    Teste de classificao e soma

--- algoritmo_classificacao_carro.py
+++ algoritmo_classificacao_carro.py
@@ -8,7 +8,53 @@ tabela.columns = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', '
 
 print(tabela)
 
+quantidade_unacc = 0
+quantidade_acc = 0
+quantidade_good = 0
+quantidade_vgood = 0
+
+
 for linha in tabela.itertuples():
-    print(linha)
-    for valor in linha[1:-1]:
-        print(valor)
+    #print(linha)
+    if linha.buying == 'vhigh' and linha.maint == 'vhigh' or linha.maint == 'high':
+        quantidade_unacc += 1
+    elif linha.buying == 'vhigh' and linha.maint == 'med' and linha.safety == 'low':
+        quantidade_unacc += 1
+    elif linha.buying == 'vhigh' and linha.maint == 'med':
+        quantidade_acc += 1
+    elif linha.buying == 'high' and linha.safety == 'low':
+        quantidade_unacc += 1
+    elif linha.buying == 'high' and linha.safety == 'high' or linha.safety == 'med':
+        quantidade_acc += 1
+    elif linha.buying == 'med' and linha.safety == 'low':
+        quantidade_unacc += 1
+    elif linha.buying == 'med' and linha.lug_boot == 'med' and linha.safety == 'high' or linha.safety == 'med':
+        quantidade_acc += 1
+    elif linha.buying == 'med' and linha.maint == 'med' and linha.safety == 'high' or linha.safety == 'med':
+        quantidade_acc += 1
+    elif linha.buying == 'med' and linha.lug_boot == 'big' and linha.safety == 'high':
+        quantidade_vgood += 1
+    elif linha.buying == 'med' and linha.maint == 'low' and linha.lug_boot == 'big' or linha.lug_boot == 'small' and linha.safety == 'low':
+        quantidade_unacc += 1
+    elif linha.buying == 'med' and linha.maint == 'low' and linha.lug_boot == 'big' and linha.safety == 'med':
+        quantidade_good += 1
+    elif linha.buying == 'med' and linha.maint == 'vhigh' and linha.lug_boot == 'small' and linha.safety == 'low':
+        quantidade_unacc += 1
+    elif linha.buying == 'low' and linha.maint == 'vhigh' and linha.lug_boot == 'big' or linha.lug_boot == 'med' and linha.safety == 'high' or linha.safety == 'med':
+        quantidade_acc += 1
+    elif linha.buying == 'low' and linha.maint == 'high' and linha.lug_boot == 'small' and linha.safety == 'low':
+        quantidade_unacc += 1
+    elif linha.buying == 'low' and linha.maint == 'high' and linha.lug_boot == 'big' and linha.lug_boot == 'big' or linha.lug_boot == 'med' and linha.safety == 'high' or linha.safety == 'med':
+        quantidade_acc += 1
+    elif linha.buying == 'low' and linha.maint == 'med' and linha.lug_boot == 'small' and linha.safety == 'med' or linha.safety == 'high':
+        quantidade_unacc += 1
+    elif linha.buying == 'low' and linha.maint == 'med' and linha.lug_boot == 'big' or linha.lug_boot == 'med' and linha.safety == 'big' or linha.safety == 'med':
+        quantidade_good += 1
+    elif linha.buying == 'low' and linha.maint == 'med' and linha.lug_boot == 'big' and linha.safety == 'high':
+        quantidade_vgood += 1
+
+
+print("Quantidade unacc: " + str(quantidade_unacc))
+print("Quantidade acc: " + str(quantidade_acc))
+print("Quantidade vgood: " + str(quantidade_vgood))
+print("Quantidade good: " + str(quantidade_good))
