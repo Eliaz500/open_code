514542ab36925141fe0c25f82410f934e8d4016a
Eliaz500 <eliazzlima@gmail.com>
2023-11-12 02:31:45 -0300

    Calculo holdout 100-fold banco de dados wine com cálculos

--- holdout_100_cross_validation.py
+++ holdout_100_cross_validation.py
@@ -1,10 +1,8 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
-
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score
 from scipy import stats
 
 
@@ -23,9 +21,9 @@ print(dados_ultima_coluna)
 holdout = 100
 
 #Vetores para armazenar as diferentes taxa de acertos
-taxa_acerto = []
-taxa_acerto_completo = []
-taxa_acerto_sem_coluna = []
+lista_diferenca_taxa_acerto = []
+lista_taxa_acerto_completo = []
+lista_taxa_acerto_sem_coluna = []
 
 
 for _ in range(holdout):
@@ -46,11 +44,71 @@ for _ in range(holdout):
 
     # Treina o classificador com todos os dados
     knn_completo = KNeighborsClassifier(n_neighbors=1, metric='euclidean')
-    knn_completo.flit(X_train_completo, y_train_completo)
+    knn_completo.fit(X_train_completo, y_train_completo)
 
     # Previsões dos dados completos
     y_predicao_completo = knn_completo.predict(X_test_completo)
     taxa_acerto_completo = accuracy_score(y_test_completo, y_predicao_completo)
+
+    # Treina o classificador com todos os dados
+    knn_incompleto = KNeighborsClassifier(n_neighbors=1, metric='euclidean')
+    knn_incompleto.fit(X_train_incompleto, y_train_incompleto)
+
+    # Previsões dos dados completos
+    y_predicao_incompleto = knn_incompleto.predict(X_test_incompleto)
+    taxa_acerto_incompleto = accuracy_score(y_test_incompleto, y_predicao_incompleto)
+
+    diferenca_taxa = taxa_acerto_completo - taxa_acerto_incompleto
+    lista_diferenca_taxa_acerto.append(diferenca_taxa)
+
+#Calcula a diferença
+diferenca = sum(lista_diferenca_taxa_acerto) / len(lista_diferenca_taxa_acerto)
+
+print(f'\n A média das diferenças')
+print(diferenca)
+
+intervalo_confianca = stats.t.interval(
+    0.95,
+    len(lista_diferenca_taxa_acerto) - 1,
+    loc=np.mean(lista_diferenca_taxa_acerto),
+    scale=stats.sem(lista_diferenca_taxa_acerto)
+)
+
+print(f'\n Intervalo de Confiança')
+print(intervalo_confianca)
+
+# Teste de Hipotese
+estatistica, valor = stats.ttest_1samp(lista_diferenca_taxa_acerto, popmean=0)
+
+# Nível de Significância
+significancia = 0.05
+
+if valor < significancia:
+    print('Rejeita H0: A diferença da taxa de acerto é significativa entre as duas versões.')
+else:
+    print('Não Rejeita H0: Não há evidências suficientes para concluir que a diferença da taxa de acerto é significativa.')
+
+# Intervalo de confiança da taxa acerto Dados Completos
+intervalo_confiança_completo = stats.t.interval(
+    0.95,
+    len(y_test_completo) - 1,
+    loc=taxa_acerto_completo,
+    scale=stats.sem(y_test_completo)
+)
+
+print('\nIntervalo de confiança dos Dados completos')
+print(intervalo_confiança_completo)
+
+# Intervalo de confiança da taxa acerto Dados Inompletos
+intervalo_confiança_incompleto = stats.t.interval(
+    0.95,
+    len(y_test_incompleto) - 1,
+    loc=taxa_acerto_incompleto,
+    scale=stats.sem(y_test_incompleto)
+)
+
+print('\nIntervalo de confiança dos Dados Incompletos')
+print(intervalo_confiança_incompleto)
 
 
 
